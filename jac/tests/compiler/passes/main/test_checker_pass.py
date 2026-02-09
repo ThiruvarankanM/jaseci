09973b22443eddabd0b53defe1608c59c375fea8
ThiruvarankanM <thiru07official@gmail.com>
2026-02-09 23:34:14 +0530

    Fix mypy errors

--- jac/tests/compiler/passes/main/test_checker_pass.py
+++ jac/tests/compiler/passes/main/test_checker_pass.py
@@ -357,6 +357,9 @@ def test_class_construct(fixture_path: Callable[[str], str]) -> None:
     assert len(program.errors_had) == 3
 
     square_sym = mod.sym_tab.lookup("Square")
+    assert square_sym is not None
+    assert square_sym.decl is not None
+    assert square_sym.decl.type is not None
     assert square_sym.decl.type.shared is not None
     mro_class_names = [
         cls.shared.class_name
